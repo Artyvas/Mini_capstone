61289b392412cef94f3daee6d10eecf9ecb3dc0e
“Artyvas” <artemvasily@gmail.com>
2021-01-22 10:32:16 -0800

    Added some model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ApplicationRecord
-  
+
+  def is_discounted?
+    price <= 10
+    # Why is the discount price a fixed number? Strange... I would imagine that it would instead be a variable that is set equal to the original price. 
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+
   
 end
