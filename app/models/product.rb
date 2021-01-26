286db85496b85de973c476864cd5cf8155887260
“Artyvas” <artemvasily@gmail.com>
2021-01-25 17:51:19 -0800

    Adds orders controller, routes, and orders model. Still needs data validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,7 @@
 class Product < ApplicationRecord
-
   def is_discounted?
     price <= 10
-    # Why is the discount price a fixed number? Strange... I would imagine that it would instead be a variable that is set equal to the original price. 
+    # Why is the discount price a fixed number? Strange... I would imagine that it would instead be a variable that is set equal to the original price.
   end
 
   def tax
@@ -13,10 +12,19 @@ class Product < ApplicationRecord
     price + tax
   end
 
-    # def supplier
-    #   Supplier.find_by(id: supplier_id)
-    # end
-  
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+
+  validates :name, presence: true
+  validates :price, numericality: true
+  validates :price, numericality: { :greater_than => 0 }
+  validates :description, length: { minimum: 2 }
+  validates :description, length: { maximum: 500 }
+  # validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i, :message => "File must be in a valid format (.jpg, .png, jpeg)"
+  # ^ This code "presents a security risk" - need to figure out a way to write this without causing the app to crash.
+  validates :quantity, :numericality => { :only_integer => true }
 
   belongs_to :supplier
+  has_many :orders
 end
